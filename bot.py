98cd980a57288b0eb9309697818eb600640db2e1
Pedro-Brito-09 <pe.brito09@gmail.com>
2026-01-27 20:56:59 -0300

    fix: adding bot variable

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ TOKEN = os.getenv("TOKEN") # we will put this in railway later
 intents = discord.Intents.default()
 intents.message_content = True
 
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
